5a256410b55b93da73cf5534680dc198d2a841a2
Mohamed Burhan <bern2727@gmail.com>
2023-04-03 00:54:12 +0800

    FEAT: Logout page
    1. Easy, just unset the sessions

--- module_c/logout.php
+++ module_c/logout.php
@@ -1,3 +1,13 @@
-<?php  
-  echo "Hello, I am logout";
-?>  
+<?php
+session_start();
+
+// Unset all of the session variables
+session_unset();
+
+// Destroy the session data that is currently stored on the server
+session_destroy();
+
+// Redirect the user to the login page
+header('Location: login.php');
+exit;
+?>
