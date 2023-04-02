62a1e1277bb3c3751e0cadacc0fd109c8fbb7b8a
Mohamed Burhan <bern2727@gmail.com>
2023-04-02 23:30:52 +0800

    FEAT: Basic Login page done

--- module_c/login.php
+++ module_c/login.php
@@ -1,3 +1,21 @@
-<?php  
-  echo "Hello, i am login";
-?>  
+<?php
+  // PHP Code goes here
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username"><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Login">
+        <input type="reset" value="Reset">
+    </form>
+</body>
+</html>
