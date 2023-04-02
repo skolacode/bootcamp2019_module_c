279c553b758255555d64000404c0cbc3a765e608
Mohamed Burhan <bern2727@gmail.com>
2023-04-02 23:38:29 +0800

    FEAT: Added a jquery and start the validations
    1. Added the jquery file
    2. implement focus feature

--- module_c/login.php
+++ module_c/login.php
@@ -1,11 +1,13 @@
 <?php
-  // PHP Code goes here
+  // PHP code goes here
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login Form</title>
+
+    <script src="lib/jquery-3.3.1.slim.min.js"></script>
 </head>
 <body>
     <h1>Login</h1>
@@ -17,5 +19,17 @@
         <input type="submit" value="Login">
         <input type="reset" value="Reset">
     </form>
+
+    <script>
+      // focus on the username input field when the page is loaded
+      $(document).ready(function() {
+          $('#username').focus();
+      });
+
+      // focus on the username input field after the reset button is clicked
+      $('input[type="reset"]').on('click', function() {
+            $('#username').focus();
+        });
+    </script>
 </body>
 </html>
