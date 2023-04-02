4bc4e13afb5ee5da195c74b3403c891050932957
Mohamed Burhan <bern2727@gmail.com>
2023-04-02 23:58:05 +0800

    FEAT: Done the error msg validation

--- module_c/login.php
+++ module_c/login.php
@@ -1,5 +1,41 @@
 <?php
-  // PHP code goes here
+
+  // Hold the value of input
+  $username = '';
+  $password = '';
+
+  $usernameErr = '';
+  $passwordErr = '';
+
+  // handle form submission if it has been submitted
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    // trim input
+    // - Password value should not be trimmed, the space might be the password
+    // ---
+    
+    // trim username
+    $username = trim($username);
+
+
+    if (empty($username)) {
+      $usernameErr = 'Username is required';
+    }
+
+    if (empty($password)) {
+      $passwordErr = 'Password is required';
+    }
+
+    /**
+     * TODO:
+     * 1. 2 more validation error
+     * 2. one for username, if it is not found
+     * 3. one for password, if not match
+     */
+
+  }
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +49,12 @@
     <h1>Login</h1>
     <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username"><br><br>
+        <input type="text" id="username" name="username" value="<?php echo $username; ?>">
+        <span class="error">* <?php echo $usernameErr;?></span><br><br>
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password"><br><br>
+        <!-- Password value should not be retain -->
+        <input type="password" id="password" name="password">
+        <span class="error">* <?php echo $passwordErr;?></span><br><br>
         <input type="submit" value="Login">
         <input type="reset" value="Reset">
     </form>
